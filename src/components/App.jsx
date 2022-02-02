39bbabebfcdef7355cd8338837c910be2fbe9e96
McBarbar <42481948+StanMc@users.noreply.github.com>
2022-02-02 05:58:21 -0800

    first challenge completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+
+  function onChangeFirstName(event) {
+    console.log("onChangeFirstName()...");
+    setFirstName(event.target.value);
+    console.log(firstName);
+  }
+
+  function onChangeLastName(event) {
+    console.log("onChangeLastName()...");
+    setLastName(event.target.value);
+  }
+
+  function onClickHandler(event) {
+    console.log("onClickHandler()...");
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello</h1>
+      <h1>
+        Hello {firstName} {lastName}
+      </h1>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <button>Submit</button>
+        <input
+          name="fName"
+          value={firstName}
+          onChange={onChangeFirstName}
+          placeholder="First Name"
+        />
+        <input
+          name="lName"
+          value={lastName}
+          onChange={onChangeLastName}
+          placeholder="Last Name"
+        />
+        <button onClick={onClickHandler}>Submit</button>
       </form>
     </div>
   );
